65a75509d8545bbc8f1debe13d002d0a924a5c83
Daniel Collingwood <82693586+danzuep@users.noreply.github.com>
2026-03-29 20:26:53 +0800

    Remove MAUI SchedulePlanner.App

--- source/SchedulePlanner.App/AppShell.xaml.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SchedulePlanner.App;
-
-public partial class AppShell : Shell
-{
-	public AppShell()
-	{
-		InitializeComponent();
-	}
-}
--- source/SchedulePlanner.AppHost/AppHost.cs
+++
@@ -1,5 +0,0 @@
-var builder = DistributedApplication.CreateBuilder(args);
-
-builder.AddProject<Projects.SchedulePlanner_App>("scheduleplanner-app");
-
-builder.Build().Run();
